95ec28974c478fb8df14137702b802a7beb9ff90
Srikanth Palakuri <44781857+srkt331@users.noreply.github.com>
2024-07-03 20:58:30 +0530

    Update apppp.py

--- apppp.py
+++ apppp.py
@@ -1,5 +1,4 @@
-streamlit install matplotlib seaborn numpy pandas scikit-learn tensorflow plotly streamlit
-
+pip install -r requirements.txt
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
